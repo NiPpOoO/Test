5e29d90eb7803beaa8eaab828c815a363d758df7
NiPpOoOz <82100496+NiPpOoO@users.noreply.github.com>
2025-12-14 16:52:06 +0300

    1

--- dist/src/index.simd.js
+++
@@ -1,5 +0,0 @@
-import ARnft from "./ARnft.simd";
-export default {
-    ARnft,
-};
-//# sourceMappingURL=index.simd.js.map
--- types/src/index.simd.d.ts
+++
@@ -1,5 +0,0 @@
-import ARnft from "./ARnft.simd";
-declare const _default: {
-    ARnft: typeof ARnft;
-};
-export default _default;
